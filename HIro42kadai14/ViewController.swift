78357e4ecb6993abef8584901ca990fd90527bf9
hiroyuki0402 <88645049+hiroyuki0402@users.noreply.github.com>
2021-12-01 10:55:37 +0900

    Initial Commit

---
+++ HIro42kadai14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HIro42kadai14
+//
+//  Created by 白石裕幸 on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HIro42kadai14Tests/HIro42kadai14Tests.swift
@@ -0,0 +1,33 @@
+//
+//  HIro42kadai14Tests.swift
+//  HIro42kadai14Tests
+//
+//  Created by 白石裕幸 on 2021/12/01.
+//
+
+import XCTest
+@testable import HIro42kadai14
+
+class HIro42kadai14Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
