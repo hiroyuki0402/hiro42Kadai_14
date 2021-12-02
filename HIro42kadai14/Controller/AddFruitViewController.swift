a3385f92d418a446fc3d5188b248aea840dea003
akio0911 <akio0911@gmail.com>
2021-12-02 20:53:25 +0900

    feedback

--- HIro42kadai14/Controller/AddFruitViewController.swift
+++ HIro42kadai14/Controller/AddFruitViewController.swift
@@ -7,16 +7,17 @@
 
 import UIKit
 
-protocol GetFruitDelegate: AnyObject {
+protocol AddFruitViewControllerDelegate: AnyObject {
     func getFruit(checkItem: CheckItem)
 }
 
 class AddFruitViewController: UIViewController {
     @IBOutlet private weak var enterFruitTextField: UITextField!
-    weak var delegate: GetFruitDelegate?
+
+    weak var delegate: AddFruitViewControllerDelegate?
+
     @IBAction private func executeSaveButton(_ sender: Any) {
-        guard let text = enterFruitTextField.text else { return }
+        guard let text = enterFruitTextField.text, !text.isEmpty else { return }
         delegate?.getFruit(checkItem: .init(name: text, isChecked: false))
-        navigationController?.popViewController(animated: true)
     }
 }
--- HIro42kadai14/Controller/ViewController.swift
+++ HIro42kadai14/Controller/ViewController.swift
@@ -5,12 +5,18 @@
 //  Created by 白石裕幸 on 2021/12/01.
 //
 import UIKit
-class ViewController: UIViewController, GetFruitDelegate {
+class ViewController: UIViewController, AddFruitViewControllerDelegate {
     @IBOutlet private weak var tableView: UITableView!
     private var fruitsInStock = FruistList().fruitsInStock
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.allowsSelection = false
+    }
+
     func getFruit(checkItem: CheckItem) {
-        fruitsInStock.append(CheckItem(name: checkItem.name, isChecked: checkItem.isChecked))
+        dismiss(animated: true, completion: nil)
+        fruitsInStock.append(checkItem)
         tableView.reloadData()
     }
 
@@ -20,7 +26,8 @@ class ViewController: UIViewController, GetFruitDelegate {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Homesegue" {
-            guard let addFruitVC = segue.destination as? AddFruitViewController else { return }
+            guard let navigationController = segue.destination as? UINavigationController else { return }
+            guard let addFruitVC = navigationController.topViewController as? AddFruitViewController else { return }
             addFruitVC.delegate = self
         }
     }
@@ -30,19 +37,19 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fruitsInStock.count
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
                 as? FruitCell else { fatalError() }
         cell.configure(item: fruitsInStock[indexPath.row])
         return cell
     }
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.selectionStyle = .none
-    }
 }
+
 class FruitCell: UITableViewCell {
     @IBOutlet private weak var checkImgaeView: UIImageView!
     @IBOutlet private weak var fruitsNameLabel: UILabel!
+
     func configure(item: CheckItem) {
         fruitsNameLabel.text = item.name
         checkImgaeView.image = item.isChecked ? UIImage(named: "checkMark") : nil
